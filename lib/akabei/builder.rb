fd8952514a570d066557ff35686badf2c4e19827
Kohei Suzuki <eagletmt@gmail.com>
2014-01-15 20:03:29 +0900

    Prepare temporary directory if SRCDEST or LOGDEST is blank

--- lib/akabei/builder.rb
+++ lib/akabei/builder.rb
@@ -16,30 +16,51 @@ module Akabei
 
     def build_package(dir)
       Dir.mktmpdir do |tmp_pkgdest|
-        tmp_pkgdest = Pathname.new(tmp_pkgdest)
-        env = {
-          SRCDEST: srcdest.realpath,
-          PKGDEST: tmp_pkgdest.realpath,
-          LOGDEST: logdest.realpath,
-        }
-        @chroot_tree.makechrootpkg(dir.to_s, env)
-        tmp_pkgdest.each_child.map do |package_path|
-          begin
-            dest = pkgdest.join(package_path.basename)
-            FileUtils.cp(package_path.to_s, dest.to_s)
-            if signer
-              signer.detach_sign(dest)
+        wrap_dir(:srcdest) do
+          wrap_dir(:logdest) do
+            tmp_pkgdest = Pathname.new(tmp_pkgdest)
+            env = {
+              SRCDEST: srcdest.realpath,
+              PKGDEST: tmp_pkgdest.realpath,
+              LOGDEST: logdest.realpath,
+            }
+            @chroot_tree.makechrootpkg(dir.to_s, env)
+            tmp_pkgdest.each_child.map do |package_path|
+              begin
+                dest = pkgdest.join(package_path.basename)
+                FileUtils.cp(package_path.to_s, dest.to_s)
+                if signer
+                  signer.detach_sign(dest)
+                end
+                Package.new(dest)
+              rescue => e
+                begin
+                  dest.unlink
+                rescue Errno::ENOENT
+                end
+                raise e
+              end
             end
-            Package.new(dest)
-          rescue => e
-            begin
-              dest.unlink
-            rescue Errno::ENOENT
-            end
-            raise e
           end
         end
       end
+    end
+
+    def wrap_dir(attr, &block)
+      old_dir = instance_variable_get("@#{attr}")
+      if old_dir.nil?
+        Dir.mktmpdir do |tmp|
+          dir = Pathname.new(tmp)
+          instance_variable_set("@#{attr}", dir)
+          # Must be world-executable because it is referred from inside chroot.
+          dir.chmod(0755)
+          block.call
+        end
+      else
+        block.call
+      end
+    ensure
+      instance_variable_set("@#{attr}", old_dir)
     end
 
     def with_source_package(dir, &block)
@@ -48,25 +69,27 @@ module Akabei
         tmp_srcpkgdest = Pathname.new(tmp_srcpkgdest)
         Dir.mktmpdir do |builddir|
           builddir = Pathname.new(builddir)
-          env = {
-            'SRCDEST' => srcdest.realpath.to_s,
-            'SRCPKGDEST' => tmp_srcpkgdest.realpath.to_s,
-            'BUILDDIR' => builddir.realpath.to_s,
-          }
-          unless system(env, 'makepkg', '--source', chdir: dir)
-            raise Error.new("makepkg --source failed: #{dir}")
+          wrap_dir(:srcdest)  do
+            env = {
+              'SRCDEST' => srcdest.realpath.to_s,
+              'SRCPKGDEST' => tmp_srcpkgdest.realpath.to_s,
+              'BUILDDIR' => builddir.realpath.to_s,
+            }
+            unless system(env, 'makepkg', '--source', chdir: dir)
+              raise Error.new("makepkg --source failed: #{dir}")
+            end
           end
-        end
-        children = tmp_srcpkgdest.each_child.to_a
-        if children.empty?
-          raise Error.new("makepkg --source generated nothing: #{dir}")
-        elsif children.size > 1
-          raise Error.new("makepkg --source generated multiple files???: #{dir}: #{children.map(&:to_s)}")
-        else
-          srcpkg = children.first
-          # Remove symlink created by makepkg
-          dir.join(srcpkg.basename).unlink
-          block.call(srcpkg)
+          children = tmp_srcpkgdest.each_child.to_a
+          if children.empty?
+            raise Error.new("makepkg --source generated nothing: #{dir}")
+          elsif children.size > 1
+            raise Error.new("makepkg --source generated multiple files???: #{dir}: #{children.map(&:to_s)}")
+          else
+            srcpkg = children.first
+            # Remove symlink created by makepkg
+            dir.join(srcpkg.basename).unlink
+            block.call(srcpkg)
+          end
         end
       end
     end
