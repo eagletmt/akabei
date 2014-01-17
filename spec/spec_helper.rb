a926d6494f052fc6541fe218f3d39afff33ca00d
Kohei Suzuki <eagletmt@gmail.com>
2014-01-17 17:40:00 +0900

    Randomize example order

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -90,7 +90,7 @@ RSpec.configure do |config|
   # order dependency and want to debug it, you can fix the order by providing
   # the seed, which is printed after each run.
   #     --seed 1234
-  #config.order = 'random'
+  config.order = 'random'
 
   config.include TestDataHelpers
   config.before(:each) do
