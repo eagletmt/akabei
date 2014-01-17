89a90402769e83e226f03d17e0b9f0f636880663
Kohei Suzuki <eagletmt@gmail.com>
2014-01-17 17:01:07 +0900

    Add PackageInfo.parse spec

--- spec/package_info_spec.rb
+++ spec/package_info_spec.rb
@@ -2,4 +2,16 @@ require 'spec_helper'
 require 'akabei/package'
 
 describe Akabei::PackageInfo do
+  describe '.parse' do
+    it 'parses .PKGINFO' do
+      pkginfo = described_class.parse(test_input('ruby.PKGINFO').read)
+      expect(pkginfo.pkgver).to eq('2.1.0-2')
+      expect(pkginfo.pkgbase).to eq('ruby')
+      expect(pkginfo.provides).to match_array(%w[rake rubygems])
+      expect(pkginfo.group).to eq([])
+
+      pkginfo = described_class.parse(test_input('nkf.PKGINFO').read)
+      expect(pkginfo.pkgbase).to be_nil
+    end
+  end
 end
