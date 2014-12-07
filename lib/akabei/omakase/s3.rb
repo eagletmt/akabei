1668dd809f4cd04a069fc15fb1f01dcd1d66771d
Kohei Suzuki <eagletmt@gmail.com>
2014-12-07 18:48:55 +0900

    Do not depend on aws-sdk-resources

--- lib/akabei/omakase/s3.rb
+++ lib/akabei/omakase/s3.rb
@@ -3,23 +3,24 @@ module Akabei
     class S3
       def initialize(aws_config, shell)
         if aws_config
-          require 'aws-sdk-resources'
-          @bucket = Aws::S3::Resource.new(
+          require 'aws-sdk-core'
+          @s3 = Aws::S3::Client.new(
             access_key_id: aws_config['access_key_id'],
             secret_access_key: aws_config['secret_access_key'],
             region: aws_config['region'],
-          ).bucket(aws_config['bucket'])
+          )
+          @bucket_name = aws_config['bucket']
           @write_options = aws_config['write_options']
           @shell = shell
         end
       end
 
       def before!(config, arch)
-        download_repository(config, arch) if @bucket
+        download_repository(config, arch) if @s3
       end
 
       def after!(config, arch, packages)
-        upload_repository(config, arch, packages) if @bucket
+        upload_repository(config, arch, packages) if @s3
       end
 
       def download_repository(config, arch)
@@ -33,11 +34,11 @@ module Akabei
 
       def get(path)
         @shell.say("Download #{path}", :blue)
-        path.open('wb') do |f|
-          @bucket.object(path.to_s).get do |chunk|
-            f.write(chunk)
-          end
-        end
+        @s3.get_object(
+          bucket: @bucket_name,
+          key: path.to_s,
+          response_target: path,
+        )
       rescue Aws::S3::Errors::NoSuchKey
         @shell.say("S3: #{path} not found", :red)
         FileUtils.rm_f(path)
@@ -65,10 +66,14 @@ module Akabei
       def put(path, mime_type)
         @shell.say("Upload #{path}", :green)
         path.open do |f|
-          @bucket.object(path.to_s).put(@write_options.merge(
-            body: f,
-            content_type: mime_type,
-          ))
+          @s3.put_object(
+            @write_options.merge(
+              bucket: @bucket_name,
+              key: path.to_s,
+              body: f,
+              content_type: mime_type,
+            )
+          )
         end
       end
     end
